07f097cc0146645eaf596d7e242c568a38aa162e
Jake Rye <jrye@mit.edu>
2017-04-24 16:48:14 -0400

    added support for setpoints to be None

--- recipe_generator.py
+++ recipe_generator.py
@@ -2,34 +2,35 @@ import json
 import operator
 
 ### Set parameters #############################################################
-recipe_name = 'example_recipe'
+recipe_name = 'nutrient_doser_fail_test_7d'
 recipe_format = 'simple'
-cycles = 2 # cycles
-day_length = 18 # hours
+cycles = 84 # cycles
+day_length = 1 # hours
 light_intensity_red = 1 # 1 for on, 0 for off
 light_intensity_blue = 1 # 1 for on, 0 for off
 light_intensity_white = 1 # 1 for on, 0 for off
-night_length = 6 # hours
-day_air_temperature = 24 # C
-night_air_temperature = 16 # C
-air_flush_on_number_per_day = 2 # times / day
+night_length = 1 # hours
+day_air_temperature = None # C
+night_air_temperature = None # C
+air_flush_on_number_per_day = 0 # times / day
 air_flush_on_day_length = 60 # minutes
 air_flush_on_number_per_night = 0 # times / night
 air_flush_on_night_length = 20 # minutes
-ph = 6 # pH
-nutrient_a_number_per_day = 2 # times / day
-nutrient_a_volume = 2.5 # mL
+ph = None # pH
+nutrient_a_number_per_day = 1 # times / day
+nutrient_a_volume = 5 # mL
 nutrient_b_number_per_day = 1 # times / day
 nutrient_b_volume = 5 # mL
 ################################################################################
-
 
 # Generate recipe
 recipe = {}
 recipe['_id'] = recipe_name
 recipe['format'] = recipe_format
 recipe['operations'] = []
-recipe['operations'].append([0, 'water_potential_hydrogen', ph])
+if ph is not None:
+    recipe['operations'].append([0, 'water_potential_hydrogen', ph])
+
 for i in range(cycles):
     # Set day & night start times
     day_start_time = (day_length + night_length) * 3600 * i
@@ -38,7 +39,8 @@ for i in range(cycles):
     recipe['operations'].append([day_start_time, 'light_intensity_red', light_intensity_red])
     recipe['operations'].append([day_start_time, 'light_intensity_blue', light_intensity_blue])
     recipe['operations'].append([day_start_time, 'light_intensity_white', light_intensity_white])
-    recipe['operations'].append([day_start_time, 'air_temperature', day_air_temperature])
+    if day_air_temperature != None:
+        recipe['operations'].append([day_start_time, 'air_temperature', day_air_temperature])
     # Set air flush
     for i in range(air_flush_on_number_per_day):
         time = day_start_time + day_length * 3600 / air_flush_on_number_per_day * i
@@ -59,7 +61,9 @@ for i in range(cycles):
     recipe['operations'].append([night_start_time, 'light_intensity_red', 0])
     recipe['operations'].append([night_start_time, 'light_intensity_blue', 0])
     recipe['operations'].append([night_start_time, 'light_intensity_white', 0])
-    recipe['operations'].append([night_start_time, 'air_temperature', night_air_temperature])
+    if night_air_temperature is not None:
+        recipe['operations'].append([night_start_time, 'air_temperature', night_air_temperature])
+
     for i in range(air_flush_on_number_per_night):
         time = night_start_time + night_length * 3600 / air_flush_on_number_per_night * i
         recipe['operations'].append([time, 'air_flush_on', 0]) # Air flush only sets new value if different than prev
