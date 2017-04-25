b198b002d345ec48c538f91ecf57bd7b05a00bcc
Bob Davis <bob.davis@fenome.com>
2017-04-25 10:45:48 -0600

    Updated load_server to include upload and start recipe as separate options.

--- load_recipe_to_server.py
+++ load_recipe_to_server.py
@@ -1,17 +1,24 @@
+#!/usr/bin/python
 import couchdb
 import argparse
 import json
 import requests
 import subprocess
+import sys
+
 
 def load_args():
     parser = argparse.ArgumentParser(description="Load recipes to couchdb instance",
-                                     epilog="python load_recipe_to_server.py -u \"http://10.1.10.106:5984\" -r light_blue_red_72hours.json --start_recipe")
-    parser.add_argument('-u', '--server_url', help='Couchdb Server: http://localhost:5984  (dont forget the http or port)')
+                                     epilog="Example usage: ./load_recipe_to_server.py -a \"http://10.1.10.106:5984\" -r light_blue_red_72hours.json -u --start_recipe")
+    parser.add_argument('-a', '--server_url', help='Couchdb Server: http://localhost:5984  (dont forget the http or port)')
     parser.add_argument('-r', '--recipe', help='Recipe json file path. It should be a proper json file.')
-    parser.add_argument('-s', '--start_recipe', help='Start recipe after uploading it? (True/False) Default:False', action='store_true', default='store_false')
+    parser.add_argument('-u', '--upload_recipe', help='Upload the recipe to the server (True/False) Default:False', action='store_true', default=False)
+    parser.add_argument('-s', '--start_recipe', help='Start recipe after uploading it? (True/False) Default:False', action='store_true', default=False)
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
     args = parser.parse_args()
-    return args.server_url, args.recipe, args.start_recipe
+    return args.server_url, args.recipe, args.upload_recipe, args.start_recipe
 
 
 def validate_server_uri(server_uri):
@@ -35,11 +42,11 @@ def save_to_server(server_uri, db_name, document):
     db[document['_id']] = document
 
 
-def start_recipe_on_server(server_uri, recipe_id):
+def start_recipe_on_server(server_uri, recipe_id, environment="environment_1"):
     service_url = server_uri + "/_openag/api/0.0.1/service"
-    api_url = service_url + "/environments/environment_1/start_recipe"
+    api_url = service_url + "/environments/{}/start_recipe".format(environment)
     print(api_url)
-    #r = requests.post(api_url, params = recipe_id)
+    # r = requests.post(api_url, params = recipe_id)
     # Not sure what format the post needs to be in...
     cmd_string = "curl -H 'Content-Type: application/json' \
                        -X POST {} \
@@ -55,11 +62,13 @@ def test_start_recipe_on_server():
     assert False
 
 def main():
-    server_uri, recipe_file_name, start_recipe = load_args()
+    server_uri, recipe_file_name, upload_recipe, start_recipe = load_args()
     recipe_dict = load_recipe(recipe_file_name)
-    db_name = "recipes"
-    print("Uploading " + recipe_dict["_id"] + " ...")
-    save_to_server(server_uri, db_name, recipe_dict)
+    print(upload_recipe)
+    if server_uri and upload_recipe:
+        db_name = "recipes"
+        print("Uploading " + recipe_dict["_id"] + " ...")
+        save_to_server(server_uri, db_name, recipe_dict)
     if start_recipe:
         print("Sending command to start recipe " + recipe_dict["_id"])
         start_recipe_on_server(server_uri, recipe_dict["_id"])
