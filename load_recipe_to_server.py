c29eeffe09909d1c82c48b4861df5167829412b7
Bob Davis <bob.davis@fenome.com>
2017-04-25 13:28:11 -0600

    Added option to run recipes on multiple pfcs

--- load_recipe_to_server.py
+++ load_recipe_to_server.py
@@ -5,6 +5,8 @@ import json
 import requests
 import subprocess
 import sys
+import pdb
+import os
 
 SERVICE_BASE_URI = "/_openag/api/0.0.1/service"
 
@@ -25,13 +27,13 @@ def load_args():
     return args, parser
 
 
-def validate_server_uri(server_uri):
+def validate_server_url(server_url):
     # Need to account for ending slash
-    if not server_uri.startswith("http"):
-        server_uri = "http://" + server_uri
-    if not server_uri[-5] == ":":
-        server_uri += ":5984"
-    return server_uri
+    if not server_url.startswith("http"):
+        server_url = "http://" + server_url
+    if not server_url[-5] == ":":
+        server_url += ":5984"
+    return server_url
 
 
 def load_recipe(file_name):
@@ -40,14 +42,14 @@ def load_recipe(file_name):
     return data
 
 
-def save_to_server(server_uri, db_name, document):
-    server = couchdb.Server(server_uri)
+def save_to_server(server_url, db_name, document):
+    server = couchdb.Server(server_url)
     db = server[db_name]
     db[document['_id']] = document
 
 
-def start_recipe_on_server(server_uri=None, recipe_id=None, environment="environment_1"):
-    service_url = server_uri + SERVICE_BASE_URI
+def start_recipe_on_server(server_url=None, recipe_id=None, environment="environment_1"):
+    service_url = server_url + SERVICE_BASE_URI
     api_url = service_url + "/environments/{}/start_recipe".format(environment)
     print(api_url)
     # r = requests.post(api_url, params = recipe_id)
@@ -60,47 +62,68 @@ def start_recipe_on_server(server_uri=None, recipe_id=None, environment="environ
     # assert r.status_code == "200"
 
 
-def stop_recipe_on_server(server_uri=None, environment="environment_1"):
-    service_url = server_uri + SERVICE_BASE_URI
+def stop_recipe_on_server(server_url=None, environment="environment_1", **kwargs):
+    service_url = server_url + SERVICE_BASE_URI
     api_url = service_url + "/environments/{}/stop_recipe".format(environment)
     cmd_string = "curl -X POST {} ".format(api_url)
     print(cmd_string)
     subprocess.call(cmd_string, shell=True)
 
 
-def upload_and_start_recipe(server_uri=None,
+def upload_and_start_recipe(server_url=None,
                             upload_recipe=None,
-                            recipe_dict=None):
+                            recipe_dict=None,
+                            start_recipe=None,
+                            stop_recipe=None, **kwargs):
     print(upload_recipe)
-    if server_uri and upload_recipe:
+
+    if server_url and upload_recipe:
         db_name = "recipes"
         print("Uploading " + recipe_dict["_id"] + " ...")
-        save_to_server(server_uri, db_name, recipe_dict)
+        try:
+            save_to_server(server_url, db_name, recipe_dict)
+        except couchdb.http.ResourceConflict:
+            print("Warning: Recipe failed to upload. Already on the server. \
+            Recipe Name: {} PFC: {}".format(recipe_dict['_id'], server_url))
+
     if start_recipe:
+        if stop_recipe is not None and stop_recipe: # Stop currently running recipe
+            stop_recipe_on_server(server_url=server_url, **kwargs)
         print("Sending command to start recipe " + recipe_dict["_id"])
-        start_recipe_on_server(server_uri, recipe_dict["_id"])
+        start_recipe_on_server(server_url, recipe_dict["_id"])
 
 
-def start_recipe_on_mutiple_pfcs(server_list_file_name=None, **kwargs):
+def load_list_of_servers(file_name):
+    if not os.path.exists(file_name):
+        raise Exception("File is missing. " + file_name)
+    server_list = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            server_list.append(line)
+    return server_list
+
+
+def start_recipe_on_mutiple_pfcs(server_list=None, server_url=None, **kwargs):
     """
     Start a recipe running on a group of PFCS
     """
-    server_list = load_list_of_servers(server_list_file_name)
-    for server_uri in server_list:
-            upload_and_start_recipe(server_uri, **kwargs)
-
+    server_list = load_list_of_servers(server_list)
+    for server_url in server_list:
+            server_url = server_url.replace("\n", "")
+            server_url = server_url.replace("\r", "")
+            print("Running recipe on {}".format(server_url))
+            upload_and_start_recipe(server_url=server_url, **kwargs)
+            print("----------------\n\n")
 
 def main():
     args, parser = load_args()
     recipe_dict = load_recipe(args.recipe)
-    if 'server_list_file_name' in args and os.path.exists(args.server_list_file_name):
-        print(args)
-        start_recipe_on_mutiple_pfcs(server_list_file_name=args.server_list_file_name,
-                                     recipe_dict=recipe_dict, **args)
-    elif 'server_uri' in args:
-        upload_and_start_recipe(**args)
-    elif 'stop_recipe' in args:
-        stop_recipe_on_server(**args)
+    if args.server_list is not None and os.path.exists(args.server_list):
+        start_recipe_on_mutiple_pfcs(recipe_dict=recipe_dict, **vars(args))
+    elif args.server_url is not None:
+        upload_and_start_recipe(recipe_dict=recipe_dict, **vars(args))
+    elif args.stop_recipe is not None:
+        stop_recipe_on_server(**vars(args))
     else:
         print("Command not found.")
         print(args)
